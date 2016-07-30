88f0878e88186dcab2d6b21dfd983e95a1e29d1c
airtoxin <airtoxin@icloud.com>
2016-07-30 21:47:08 +0900

    Add documents

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # react-showcase
-React.js component that helps compoent showcases
+
+Make it easy to create your own react component showcase site.
+
+## Install
+
+`$ npm install --save react-showcase`
+
+## Usage
+
+```js
+import showcase from 'react-showcase';
+
+const myComponents = [Form, Button];
+const showcases = myComponents.map(Component => {
+  const Showcase = showcase(Component);
+  reutrn <Showcase key={Component.name}/>
+});
+
+function App() {
+  return <div>{showcases}</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('showcase'));
+```
+
+It shows...
+
+![showcaseView.png](showcaseView.png)
+
+## Api
+
+### `showcase(Component)` (default exported)
+
+This function returns showcase component of `Component` argument.
+
+### `defaultStyles` (named exported)
+
+## License
+
+MIT
--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
+export const defaultStyles = {
+  article: {
+    margin: "20px 20px 40px 20px",
+    display: "flex",
+    flexDirection: "column"
+  },
+  componentDiv: {
+    display: "flex",
+    flexDirection: "column",
+    marginBottom: 10
+  },
+  componentLabel: {
+    marginLeft: -10,
+    width: "100%"
+  },
+  propsDiv: {},
+  propsLabel: {},
+  propsUl: {
+    marginTop: -3
+  },
+  propsLi: {},
+  propsInput: {}
+};
+
 export default function showcase(Component) {
   const defaultProps = Component.defaultProps || {};
 
@@ -11,22 +35,26 @@ export default function showcase(Component) {
 
     render() {
       const Props = Object.keys(defaultProps).map(key => (
-        <input
-          key={key}
+        <li key={key} style={defaultStyles.propsLi}>{key}: <input
+          style={defaultStyles.propsInput}
           type="text"
           placeholder={`${key}=${defaultProps[key]}`}
           value={this.state[key]}
           onChange={e => this.setState({ [key]: e.target.value })}
-        />
+        /></li>
       ));
 
       return (
-        <div>
-          <h3>{Component.name}</h3>
-          <Component {...this.state} />
-          <label>props</label>
-          {Props}
-        </div>
+        <article style={defaultStyles.article}>
+          <div style={defaultStyles.componentDiv}>
+            <label style={defaultStyles.componentLabel}>{Component.name}</label>
+            <Component {...this.state} />
+          </div>
+          <div style={defaultStyles.propsDiv}>
+            <label style={defaultStyles.propsLabel}>props</label>
+            <ul style={defaultStyles.propsUl}>{Props}</ul>
+          </div>
+        </article>
       );
     }
   };
